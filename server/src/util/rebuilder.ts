7dc791f755048170ce2393d3c73b0d496e971c56
Kolja Lampe <razzeee@gmail.com>
2019-01-27 15:51:14 +0100

    Fix prebuild

--- server/src/util/rebuilder.ts
+++ server/src/util/rebuilder.ts
@@ -14,21 +14,24 @@ function packageToGithubRepo(name: string): string {
 	return repo;
 }
 
-function downloadUrl(name: string, version: string): string {
+function downloadUrl(name: string, version: string, treeSitterRepo: boolean): string {
 	const repo: string = packageToGithubRepo(name);
-	const urlBase: string = `https://github.com/tree-sitter/${repo}/releases/download/v${version}/`;
+	let urlBase: string = `https://github.com/tree-sitter/${repo}/releases/download/v${version}/`;
+	if (!treeSitterRepo) {
+		urlBase = `https://github.com/razzeee/${repo}/releases/download/v${version}/`;
+	}
 	const prebuild: string = `${name}-v${version}-electron-v${process.versions.modules}-${
 		process.platform
-	}-${process.arch}.tar.gz`;
+		}-${process.arch}.tar.gz`;
 
 	return `${urlBase}${prebuild}`;
 }
 
-function fetchPrebuild(name: string): Promise<void | Error> {
+function fetchPrebuild(name: string, treeSitterRepo: boolean): Promise<void | Error> {
 	const pkgRoot: string = path.resolve(path.join(__dirname, '../../node_modules', name));
 	//tslint:disable-next-line non-literal-require
 	const pkg: { name: string; version: string } = require(`${pkgRoot}/package.json`);
-	const url: string = downloadUrl(pkg.name, pkg.version);
+	const url: string = downloadUrl(pkg.name, pkg.version, treeSitterRepo);
 
 	return new Promise((res, rej) => {
 		prebuildInstall.download(url, { pkg, path: pkgRoot }, (err: Error) => {
@@ -38,5 +41,5 @@ function fetchPrebuild(name: string): Promise<void | Error> {
 }
 
 export function rebuildTreeSitter(): Promise<[void | Error, void | Error]> {
-	return Promise.all([fetchPrebuild('tree-sitter'), fetchPrebuild('tree-sitter-elm')]);
+	return Promise.all([fetchPrebuild('tree-sitter', true), fetchPrebuild('tree-sitter-elm', false)]);
 }
