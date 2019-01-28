09acac0690a376858fa668fc64deefde235b99f1
Kolja Lampe <razzeee@gmail.com>
2019-01-29 00:31:43 +0100

    Find elm file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,34 +1,41 @@
 import {
-    createConnection,
-    IConnection,
-    InitializeParams,
-    ProposedFeatures,
+  createConnection,
+  IConnection,
+  InitializeParams,
+  ProposedFeatures,
+  WorkspaceFolder,
 } from "vscode-languageserver";
 
 import { ILanguageServer } from "./server";
 import { rebuildTreeSitter } from "./util/rebuilder";
 
 const connection: IConnection = createConnection(ProposedFeatures.all);
+let workspaceFolders: WorkspaceFolder[];
 
 connection.onInitialize(async (params: InitializeParams) => {
-    connection.console.info("Initializing Elm language server...");
+  workspaceFolders = params.workspaceFolders;
+  connection.console.info("Initializing Elm language server...");
 
-    connection.console.info("Rebuilding tree-sitter for local Electron version");
-    const rebuildResult: [void | Error, void | Error] = await rebuildTreeSitter();
-    for (const result of rebuildResult) {
-        if (result) {
-            connection.console.error("Rebuild failed!");
-            connection.console.error(result.toString());
+  connection.console.info("Rebuilding tree-sitter for local Electron version");
+  const rebuildResult: [void | Error, void | Error] = await rebuildTreeSitter();
+  for (const result of rebuildResult) {
+    if (result) {
+      connection.console.error("Rebuild failed!");
+      connection.console.error(result.toString());
 
-            return null;
-        }
+      return null;
     }
-    connection.console.info("Rebuild succeeded!");
+  }
+  connection.console.info("Rebuild succeeded!");
 
-    const { Server } = await import("./server");
-    const server: ILanguageServer = new Server(connection, params);
+  const { Server } = await import("./server");
+  const server: ILanguageServer = new Server(
+    connection,
+    workspaceFolders,
+    params,
+  );
 
-    return server.capabilities;
+  return server.capabilities;
 });
 
 // Listen on the connection
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,12 @@
-import { Connection, InitializeParams, InitializeResult } from "vscode-languageserver";
+import {
+  Connection,
+  InitializeParams,
+  InitializeResult,
+  WorkspaceFolder,
+} from "vscode-languageserver";
 
+import fs = require("fs");
+import URI from "vscode-uri";
 import { CapabilityCalculator } from "./capabilityCalculator";
 import { Forest } from "./forest";
 import { ASTProvider } from "./providers/astProvider";
@@ -8,33 +15,51 @@ import { DiagnosticsProvider } from "./providers/diagnosticsProvider";
 import { FoldingRangeProvider } from "./providers/foldingProvider";
 
 export interface ILanguageServer {
-    readonly capabilities: InitializeResult;
+  readonly capabilities: InitializeResult;
 }
 
 export class Server implements ILanguageServer {
-    public connection: Connection;
-    private calculator: CapabilityCalculator;
-    private forest: Forest;
+  public connection: Connection;
+  public workspaceFolders: WorkspaceFolder[];
+  public elmWorkspaceFolder: URI;
+  private calculator: CapabilityCalculator;
+  private forest: Forest;
 
-    constructor(connection: Connection, params: InitializeParams) {
-        this.connection = connection;
-        this.calculator = new CapabilityCalculator(params.capabilities);
-        this.forest = new Forest();
+  constructor(
+    connection: Connection,
+    workspaceFolders: WorkspaceFolder[],
+    params: InitializeParams,
+  ) {
+    this.connection = connection;
+    this.workspaceFolders = workspaceFolders;
+    this.elmWorkspaceFolder = this.findElmWorkspace();
+    this.calculator = new CapabilityCalculator(params.capabilities);
+    this.forest = new Forest();
 
-        this.registerProviders();
+    this.registerProviders();
+  }
+
+  get capabilities(): InitializeResult {
+    return {
+      capabilities: this.calculator.capabilities,
+    };
+  }
+
+  private findElmWorkspace() {
+    const elmFile = fs.readdirSync(this.workspaceFolders[0].name).find((value) => value === "elm.json");
+    if (elmFile) {
+      return URI.parse(elmFile);
+    } else {
+      this.connection.console.error("Cannot find elm.json in workspace.");
+      return null;
     }
+  }
 
-    get capabilities(): InitializeResult {
-        return {
-            capabilities: this.calculator.capabilities,
-        };
-    }
-
-    private registerProviders(): void {
-        // tslint:disable:no-unused-expression
-        new ASTProvider(this.connection, this.forest);
-        new FoldingRangeProvider(this.connection, this.forest);
-        new CompletionProvider(this.connection, this.forest);
-        new DiagnosticsProvider(this.connection);
-    }
+  private registerProviders(): void {
+    // tslint:disable:no-unused-expression
+    new ASTProvider(this.connection, this.forest);
+    new FoldingRangeProvider(this.connection, this.forest);
+    new CompletionProvider(this.connection, this.forest);
+    new DiagnosticsProvider(this.connection, this.elmWorkspaceFolder);
+  }
 }
