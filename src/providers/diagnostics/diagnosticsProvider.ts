6309674120e187f5854da9299fc7f568240dde56
Kolja Lampe <razzeee@gmail.com>
2019-07-10 23:37:21 +0200

    Remove some dead code

--- src/providers/diagnostics/diagnosticsProvider.ts
+++ src/providers/diagnostics/diagnosticsProvider.ts
@@ -1,12 +1,5 @@
-import {
-  Diagnostic,
-  DiagnosticSeverity,
-  IConnection,
-  Range,
-  TextDocument,
-} from "vscode-languageserver";
+import { Diagnostic, IConnection, TextDocument } from "vscode-languageserver";
 import { URI } from "vscode-uri";
-import * as utils from "../../util/elmUtils";
 import { Settings } from "../../util/settings";
 import { TextDocumentEvents } from "../../util/textDocumentEvents";
 import { ElmAnalyseDiagnostics } from "./elmAnalyseDiagnostics";
@@ -52,7 +45,6 @@ export class DiagnosticsProvider {
     );
     this.getDiagnosticsOnChange = this.getDiagnosticsOnChange.bind(this);
     this.newElmAnalyseDiagnostics = this.newElmAnalyseDiagnostics.bind(this);
-    this.elmMakeIssueToDiagnostic = this.elmMakeIssueToDiagnostic.bind(this);
     this.events = events;
     this.elmAnalyseDiagnostics = elmAnalyse;
 
@@ -154,43 +146,6 @@ export class DiagnosticsProvider {
       }
 
       this.sendDiagnostics();
-    }
-  }
-
-  private elmMakeIssueToDiagnostic(issue: IElmIssue): Diagnostic {
-    const lineRange: Range = Range.create(
-      issue.region.start.line === 0
-        ? issue.region.start.line
-        : issue.region.start.line - 1,
-      issue.region.start.column === 0
-        ? issue.region.start.column
-        : issue.region.start.column - 1,
-      issue.region.end.line === 0
-        ? issue.region.end.line
-        : issue.region.end.line - 1,
-      issue.region.end.column === 0
-        ? issue.region.end.column
-        : issue.region.end.column - 1,
-    );
-    return Diagnostic.create(
-      lineRange,
-      `${issue.overview} - ${issue.details.replace(/\[\d+m/g, "")}`,
-      this.severityStringToDiagnosticSeverity(issue.type),
-      undefined,
-      "Elm",
-    );
-  }
-
-  private severityStringToDiagnosticSeverity(
-    severity: string,
-  ): DiagnosticSeverity {
-    switch (severity) {
-      case "error":
-        return DiagnosticSeverity.Error;
-      case "warning":
-        return DiagnosticSeverity.Warning;
-      default:
-        return DiagnosticSeverity.Error;
     }
   }
 }
