20e5c4ef22d69b1f7a130e48876cdb0af1defbb6
Razze <razzeee@gmail.com>
2021-01-10 12:43:28 +0100

    Don't use elm-tooling-cli download for tests

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -2,13 +2,19 @@ import "reflect-metadata";
 import { container } from "tsyringe";
 import { Connection } from "vscode-languageserver";
 import { mockDeep } from "jest-mock-extended";
-import { Settings } from "../src/util/settings";
+import { IClientSettings, Settings } from "../src/util/settings";
 import { DocumentEvents } from "../src/util/documentEvents";
 
 container.register("Connection", { useValue: mockDeep<Connection>() });
 container.register("ElmWorkspaces", { useValue: [] });
 container.register("Settings", {
-  useValue: new Settings({} as any, {}),
+  useValue: new Settings(
+    {
+      // skip this code path, as mocking the promise from showInformationMessage is no fun
+      useElmToolingJsonForTools: false,
+    } as IClientSettings,
+    {},
+  ),
 });
 container.register("ClientSettings", {
   useValue: {},
