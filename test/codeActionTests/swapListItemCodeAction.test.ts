d944e2e2f163dcb5206b54f638b4d0e0b4605201
Nils Henrik Hals <nilshenrik91@gmail.com>
2021-02-28 19:42:32 +0100

    Add broken test

---
+++ test/codeActionTests/swapListItemCodeAction.test.ts
@@ -0,0 +1,30 @@
+import { testCodeAction } from "./codeActionTestBase";
+
+describe("swap list item code action", () => {
+  it("should swap item aa with bb", async () => {
+    const source = `
+--@ Test.elm
+module Test exposing (..)
+
+func =
+    [ "aa", "bb", "cc" ]
+    --^
+
+`;
+
+    const expectedSource = `
+--@ Test.elm
+module Test exposing (..)
+
+func =
+    [ "bb", "aa", "cc" ]
+
+`;
+
+    await testCodeAction(
+      source,
+      [{ title: "Move List item Down" }],
+      expectedSource,
+    );
+  });
+  
