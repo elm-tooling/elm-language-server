bde5408c788625941078c36544b9b147e2ad7976
Jon Bockhorst <jmbockhorst@gmail.com>
2021-03-04 13:34:55 -0600

    Add function with args references tests

--- test/referencesProviderTests/functionReferences.test.ts
+++ test/referencesProviderTests/functionReferences.test.ts
@@ -32,6 +32,35 @@ func = foo
     await testBase.testReferences(source);
   });
 
+  it(`function with args references in other files`, async () => {
+    const source = `
+--@ Module.elm
+module Module exposing (foo)
+                       --X
+foo a b = 42
+--^
+
+--@ Bar.elm
+module Bar exposing (..)
+
+import Module exposing (foo)
+                       --X
+
+bar = foo 1 2
+     --X
+
+--@ FooExtra.elm
+module FooExtra exposing (..)
+
+import Module exposing (foo)
+                       --X
+
+func = foo 1 2
+      --X
+`;
+    await testBase.testReferences(source);
+  });
+
   it(`function references used with module prefix in other files`, async () => {
     const source = `
 --@ Module.elm
@@ -54,6 +83,33 @@ module FooExtra exposing (..)
 import Module
 
 func = Module.foo
+             --X
+`;
+    await testBase.testReferences(source);
+  });
+
+  it(`function with arg references used with module prefix in other files`, async () => {
+    const source = `
+--@ Module.elm
+module Module exposing (foo)
+                       --X
+foo a b = 42
+--^
+
+--@ Bar.elm
+module Bar exposing (..)
+
+import Module
+
+bar = Module.foo 1 2
+           --X
+
+--@ FooExtra.elm
+module FooExtra exposing (..)
+
+import Module
+
+func = Module.foo 1 2
              --X
 `;
     await testBase.testReferences(source);
@@ -95,6 +151,20 @@ foo = 42
 --^
 
 bar = foo
+     --X
+`;
+    await testBase.testReferences(source);
+  });
+
+  it(`local calls to a function with args`, async () => {
+    const source = `
+--@ Module.elm
+module Module exposing (foo)
+                        --X
+foo a = 42
+--^
+
+bar = foo 1
      --X
 `;
     await testBase.testReferences(source);
@@ -155,6 +225,37 @@ foo =
 test = 
     foo.field
    --X
+`;
+    await testBase.testReferences(source);
+  });
+
+  it(`function references used both with and without module prefix in other files`, async () => {
+    const source = `
+--@ Module.elm
+module Module exposing (foo)
+                       --X
+foo = 42
+--^
+
+--@ Bar.elm
+module Bar exposing (..)
+
+import Module
+
+bar = Module.foo
+           --X
+
+--@ FooExtra.elm
+module FooExtra exposing (..)
+
+import Module exposing (foo)
+                       --X
+
+func = Module.foo
+             --X
+
+func2 = foo
+       --X
 `;
     await testBase.testReferences(source);
   });
