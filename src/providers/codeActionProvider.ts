bd422fab67be50ea639fdbf3898b089f35b0afd7
IzumiSy <beetle-noise@gmx.com>
2022-06-30 17:30:53 +0900

    Formatting

--- src/providers/codeActionProvider.ts
+++ src/providers/codeActionProvider.ts
@@ -230,8 +230,8 @@ export class CodeActionProvider {
     const changes = callbackChanges
       ? {}
       : {
-          [params.sourceFile.uri]: edits,
-        };
+        [params.sourceFile.uri]: edits,
+      };
 
     const diagnostics: Diagnostic[] = [];
     CodeActionProvider.forEachDiagnostic(params, errorCodes, (diagnostic) => {
