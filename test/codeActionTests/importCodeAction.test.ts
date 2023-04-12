124a2dced0561b17e9d017e7deaa56f7c3e1004a
Jon Bockhorst <jbockhorst@engagesoftware.com>
2023-04-11 23:23:17 -0500

    Add import code actions for module aliases

--- test/codeActionTests/importCodeAction.test.ts
+++ test/codeActionTests/importCodeAction.test.ts
@@ -47,6 +47,54 @@ foo = ""
     await testCodeAction(source2, [{ title: `Import module "App"` }]);
   });
 
+  test("add import alias of qualified value", async () => {
+    const source = `
+--@ Test.elm
+module Test exposing (..)
+
+func = Foo.foo
+          --^
+
+--@ App/Foo.elm
+module App.Foo exposing (foo)
+
+foo = ""
+`;
+    await testCodeAction(source, [
+      { title: `Import module "App.Foo" as "Foo"` },
+    ]);
+
+    const source2 = `
+--@ Test.elm
+module Test exposing (..)
+
+func = Bar.foo
+      --^
+
+--@ App/Foo/Bar.elm
+module App.Foo.Bar exposing (foo)
+
+foo = ""
+`;
+    await testCodeAction(source2, [
+      { title: `Import module "App.Foo.Bar" as "Bar"` },
+    ]);
+
+    const source3 = `
+--@ Test.elm
+module Test exposing (..)
+
+func = Foo.foo
+      --^
+
+--@ App/Foo/Bar.elm
+module App.Foo.Bar exposing (foo)
+
+foo = ""
+    `;
+    await testCodeAction(source3, []);
+  });
+
   test("add all missing imports", async () => {
     const source = `
 --@ Test.elm
