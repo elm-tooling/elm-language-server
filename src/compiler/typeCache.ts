d98ccd27bf4e34673d4c95d4a8ba1e6c56e87f51
Jon Bockhorst <jbockhorst@engagesoftware.com>
2023-04-29 02:13:13 -0500

    Fix type inference bug with type alias variables

--- src/compiler/typeCache.ts
+++ src/compiler/typeCache.ts
@@ -8,18 +8,23 @@ type CacheKey =
   | "PACKAGE_TYPE_ANNOTATION"
   | "PACKAGE_TYPE_AND_TYPE_ALIAS"
   | "PACKAGE_VALUE_DECLARATION"
+  | "PACKAGE_UNION_VARIANT"
   | "PROJECT_TYPE_ANNOTATION"
   | "PROJECT_TYPE_AND_TYPE_ALIAS"
-  | "PROJECT_VALUE_DECLARATION";
+  | "PROJECT_VALUE_DECLARATION"
+  | "PROJECT_UNION_VARIANT";
 
 export class TypeCache {
   private packageTypeAnnotation: SyntaxNodeMap<SyntaxNode, InferenceResult>;
   private packageTypeAndTypeAlias: SyntaxNodeMap<SyntaxNode, InferenceResult>;
   private packageValueDeclaration: SyntaxNodeMap<SyntaxNode, InferenceResult>;
+  private packageUnionVariant: SyntaxNodeMap<SyntaxNode, InferenceResult>;
   private projectTypeAnnotation: SyntaxNodeMap<SyntaxNode, InferenceResult>;
   private projectTypeAndTypeAlias: SyntaxNodeMap<SyntaxNode, InferenceResult>;
   private projectValueDeclaration: SyntaxNodeMap<SyntaxNode, InferenceResult>;
+  private projectUnionVariant: SyntaxNodeMap<SyntaxNode, InferenceResult>;
   private declarationAnnotations: MultiMap<number, SyntaxNode>;
+  private typeUnionVariants: MultiMap<number, SyntaxNode>;
 
   constructor() {
     this.packageTypeAnnotation = new SyntaxNodeMap<
@@ -34,6 +39,7 @@ export class TypeCache {
       SyntaxNode,
       InferenceResult
     >();
+    this.packageUnionVariant = new SyntaxNodeMap<SyntaxNode, InferenceResult>();
     this.projectTypeAnnotation = new SyntaxNodeMap<
       SyntaxNode,
       InferenceResult
@@ -46,7 +52,10 @@ export class TypeCache {
       SyntaxNode,
       InferenceResult
     >();
+    this.projectUnionVariant = new SyntaxNodeMap<SyntaxNode, InferenceResult>();
+
     this.declarationAnnotations = new MultiMap();
+    this.typeUnionVariants = new MultiMap();
   }
 
   public getOrSet(
@@ -61,12 +70,16 @@ export class TypeCache {
         return this.packageTypeAndTypeAlias.getOrSet(node, setter);
       case "PACKAGE_VALUE_DECLARATION":
         return this.packageValueDeclaration.getOrSet(node, setter);
+      case "PACKAGE_UNION_VARIANT":
+        return this.packageUnionVariant.getOrSet(node, setter);
       case "PROJECT_TYPE_ANNOTATION":
         return this.projectTypeAnnotation.getOrSet(node, setter);
       case "PROJECT_TYPE_AND_TYPE_ALIAS":
         return this.projectTypeAndTypeAlias.getOrSet(node, setter);
       case "PROJECT_VALUE_DECLARATION":
         return this.projectValueDeclaration.getOrSet(node, setter);
+      case "PROJECT_UNION_VARIANT":
+        return this.projectUnionVariant.getOrSet(node, setter);
     }
   }
 
@@ -74,6 +87,7 @@ export class TypeCache {
     this.projectTypeAnnotation.clear();
     this.projectTypeAndTypeAlias.clear();
     this.projectValueDeclaration.clear();
+    this.projectUnionVariant.clear();
   }
 
   public invalidateValueDeclaration(node: SyntaxNode): void {
@@ -89,6 +103,9 @@ export class TypeCache {
 
   public invalidateTypeOrTypeAlias(node: SyntaxNode): void {
     this.projectTypeAndTypeAlias.delete(node);
+    this.typeUnionVariants
+      .getAll(node.id)
+      ?.forEach((variant) => this.projectUnionVariant.delete(variant));
   }
 
   /**
@@ -109,6 +126,17 @@ export class TypeCache {
 
     if (declaration) {
       this.declarationAnnotations.set(declaration.id, annotation);
+    }
+  }
+
+  public trackUnionVariant(unionVariant: SyntaxNode): void {
+    const typeDeclaration = TreeUtils.findParentOfType(
+      "type_declaration",
+      unionVariant,
+    );
+
+    if (typeDeclaration) {
+      this.typeUnionVariants.set(typeDeclaration.id, unionVariant);
     }
   }
 }
