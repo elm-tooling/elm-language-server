d086715f2eab839e7528cd071f77b74c9614436e
Jon Bockhorst <jbockhorst@engagesoftware.com>
2023-05-16 02:18:46 -0500

    Use indexed db to store package files in the browser

--- src/browser/fileSystem.ts
+++ src/browser/fileSystem.ts
@@ -8,10 +8,20 @@ import {
 } from "../common";
 import { URI } from "vscode-uri";
 import { XHRResponse, getErrorStatusDescription, xhr } from "request-light";
+import { IndexedDB } from "./indexedDB";
 
 export function createWebFileSystemHost(
   connection: Connection,
 ): IFileSystemHost {
+  let indexedDbFileSystemProvider: IIndexedDBFileSystemProvider | undefined;
+  const getFileSystemProvider =
+    async (): Promise<IIndexedDBFileSystemProvider> => {
+      if (!indexedDbFileSystemProvider) {
+        indexedDbFileSystemProvider = await getIndexedDbFileSystemProvider();
+      }
+      return indexedDbFileSystemProvider;
+    };
+
   return {
     readFile: (uri): Promise<string> =>
       readFileWithCachedVirtualPackageFile(
@@ -29,11 +39,23 @@ export function createWebFileSystemHost(
           return new TextDecoder().decode(new Uint8Array(bytes));
         },
         {
-          // TODO: Use indexed DB to store package files in the browser
-          getVirtualPackageRoot: () => uri,
-          get: (uri) => Promise.resolve(undefined),
+          getVirtualPackageRoot: () => virtualPackagesRoot,
+          get: async (uri) => {
+            try {
+              const provider = await getFileSystemProvider();
+              const result = await provider.readFile(uri);
+              return new TextDecoder().decode(result);
+            } catch {
+              return undefined;
+            }
+          },
           set: async (uri, value) => {
-            //
+            try {
+              const provider = await getFileSystemProvider();
+              await provider.writeFile(uri, new TextEncoder().encode(value));
+            } catch {
+              //
+            }
           },
         },
       ),
@@ -72,4 +94,276 @@ function loadFileFromHttp(uri: URI): Promise<string | undefined> {
       );
     },
   );
+}
+
+interface IIndexedDBFileSystemProvider {
+  readFile(resource: URI): Promise<Uint8Array>;
+  writeFile(resource: URI, content: Uint8Array): Promise<void>;
+}
+
+async function getIndexedDbFileSystemProvider(): Promise<IIndexedDBFileSystemProvider> {
+  const store = "elm-package-files";
+  const indexedDB = await IndexedDB.create("elm-language-server", 1, [store]);
+
+  const mtimes = new Map<string, number>();
+
+  let cachedFiletree: Promise<IndexedDBFileSystemNode>;
+  const getFiletree = (): Promise<IndexedDBFileSystemNode> => {
+    if (!cachedFiletree) {
+      cachedFiletree = (async (): Promise<IndexedDBFileSystemNode> => {
+        const rootNode = new IndexedDBFileSystemNode({
+          children: new Map(),
+          path: "",
+          type: FileType.Directory,
+        });
+        const result = await indexedDB.runInTransaction(
+          store,
+          "readonly",
+          (objectStore) => objectStore.getAllKeys(),
+        );
+        const keys = result.map((key) => key.toString());
+        keys.forEach((key) => rootNode.add(key, { type: "file" }));
+        return rootNode;
+      })();
+    }
+    return cachedFiletree;
+  };
+
+  const bulkWrite = async (files: [URI, Uint8Array][]): Promise<void> => {
+    files.forEach(([resource, content]) =>
+      fileWriteBatch.push({ content, resource }),
+    );
+    await writeMany();
+
+    const fileTree = await getFiletree();
+    for (const [resource, content] of files) {
+      fileTree.add(resource.path, { type: "file", size: content.byteLength });
+      mtimes.set(resource.toString(), Date.now());
+    }
+  };
+
+  const fileWriteBatch: { resource: URI; content: Uint8Array }[] = [];
+  const writeMany = async () => {
+    if (fileWriteBatch.length) {
+      const fileBatch = fileWriteBatch.splice(0, fileWriteBatch.length);
+      await indexedDB.runInTransaction(store, "readwrite", (objectStore) =>
+        fileBatch.map((entry) => {
+          return objectStore.put(entry.content, entry.resource.path);
+        }),
+      );
+    }
+  };
+
+  return {
+    async readFile(resource: URI): Promise<Uint8Array> {
+      const result = await indexedDB.runInTransaction(
+        store,
+        "readonly",
+        (objectStore) => objectStore.get(resource.path),
+      );
+      if (result === undefined) {
+        throw new Error("File not found");
+      }
+      const buffer =
+        result instanceof Uint8Array
+          ? result
+          : typeof result === "string"
+          ? new TextEncoder().encode(result)
+          : undefined;
+      if (buffer === undefined) {
+        throw Error(
+          `IndexedDB entry at "${resource.path}" in unexpected format`,
+        );
+      }
+
+      // update cache
+      const fileTree = await getFiletree();
+      fileTree.add(resource.path, { type: "file", size: buffer.byteLength });
+
+      return buffer;
+    },
+
+    async writeFile(resource: URI, content: Uint8Array): Promise<void> {
+      await bulkWrite([[resource, content]]);
+    },
+  };
+}
+
+enum FileType {
+  Unknown = 0,
+  File = 1,
+  Directory = 2,
+  SymbolicLink = 64,
+}
+
+const enum FileChangeType {
+  UPDATED,
+  ADDED,
+  DELETED,
+}
+
+interface IFileChange {
+  readonly type: FileChangeType;
+  readonly resource: URI;
+}
+
+type IndexedDBFileSystemEntry =
+  | {
+      path: string;
+      type: FileType.Directory;
+      children: Map<string, IndexedDBFileSystemNode>;
+    }
+  | {
+      path: string;
+      type: FileType.File;
+      size: number | undefined;
+    };
+
+class IndexedDBFileSystemNode {
+  public type: FileType;
+
+  constructor(private entry: IndexedDBFileSystemEntry) {
+    this.type = entry.type;
+  }
+
+  read(path: string): IndexedDBFileSystemEntry | undefined {
+    return this.doRead(path.split("/").filter((p) => p.length));
+  }
+
+  private doRead(pathParts: string[]): IndexedDBFileSystemEntry | undefined {
+    if (pathParts.length === 0) {
+      return this.entry;
+    }
+    if (this.entry.type !== FileType.Directory) {
+      throw Error(
+        "Internal error reading from IndexedDBFSNode -- expected directory at " +
+          this.entry.path,
+      );
+    }
+    const next = this.entry.children.get(pathParts[0]);
+
+    if (!next) {
+      return undefined;
+    }
+    return next.doRead(pathParts.slice(1));
+  }
+
+  delete(path: string): void {
+    const toDelete = path.split("/").filter((p) => p.length);
+    if (toDelete.length === 0) {
+      if (this.entry.type !== FileType.Directory) {
+        throw Error(
+          `Internal error deleting from IndexedDBFSNode. Expected root entry to be directory`,
+        );
+      }
+      this.entry.children.clear();
+    } else {
+      return this.doDelete(toDelete, path);
+    }
+  }
+
+  private doDelete(pathParts: string[], originalPath: string): void {
+    if (pathParts.length === 0) {
+      throw Error(
+        `Internal error deleting from IndexedDBFSNode -- got no deletion path parts (encountered while deleting ${originalPath})`,
+      );
+    } else if (this.entry.type !== FileType.Directory) {
+      throw Error(
+        "Internal error deleting from IndexedDBFSNode -- expected directory at " +
+          this.entry.path,
+      );
+    } else if (pathParts.length === 1) {
+      this.entry.children.delete(pathParts[0]);
+    } else {
+      const next = this.entry.children.get(pathParts[0]);
+      if (!next) {
+        throw Error(
+          "Internal error deleting from IndexedDBFSNode -- expected entry at " +
+            this.entry.path +
+            "/",
+        );
+      }
+      next.doDelete(pathParts.slice(1), originalPath);
+    }
+  }
+
+  add(path: string, entry: { type: "file"; size?: number } | { type: "dir" }) {
+    this.doAdd(
+      path.split("/").filter((p) => p.length),
+      entry,
+      path,
+    );
+  }
+
+  private doAdd(
+    pathParts: string[],
+    entry: { type: "file"; size?: number } | { type: "dir" },
+    originalPath: string,
+  ) {
+    if (pathParts.length === 0) {
+      throw Error(
+        `Internal error creating IndexedDBFSNode -- adding empty path (encountered while adding ${originalPath})`,
+      );
+    } else if (this.entry.type !== FileType.Directory) {
+      throw Error(
+        `Internal error creating IndexedDBFSNode -- parent is not a directory (encountered while adding ${originalPath})`,
+      );
+    } else if (pathParts.length === 1) {
+      const next = pathParts[0];
+      const existing = this.entry.children.get(next);
+      if (entry.type === "dir") {
+        if (existing?.entry.type === FileType.File) {
+          throw Error(
+            `Internal error creating IndexedDBFSNode -- overwriting file with directory: ${this.entry.path}/${next} (encountered while adding ${originalPath})`,
+          );
+        }
+        this.entry.children.set(
+          next,
+          existing ??
+            new IndexedDBFileSystemNode({
+              type: FileType.Directory,
+              path: this.entry.path + "/" + next,
+              children: new Map(),
+            }),
+        );
+      } else {
+        if (existing?.entry.type === FileType.Directory) {
+          throw Error(
+            `Internal error creating IndexedDBFSNode -- overwriting directory with file: ${this.entry.path}/${next} (encountered while adding ${originalPath})`,
+          );
+        }
+        this.entry.children.set(
+          next,
+          new IndexedDBFileSystemNode({
+            type: FileType.File,
+            path: this.entry.path + "/" + next,
+            size: entry.size,
+          }),
+        );
+      }
+    } else if (pathParts.length > 1) {
+      const next = pathParts[0];
+      let childNode = this.entry.children.get(next);
+      if (!childNode) {
+        childNode = new IndexedDBFileSystemNode({
+          children: new Map(),
+          path: this.entry.path + "/" + next,
+          type: FileType.Directory,
+        });
+        this.entry.children.set(next, childNode);
+      } else if (childNode.type === FileType.File) {
+        throw Error(
+          `Internal error creating IndexedDBFSNode -- overwriting file entry with directory: ${this.entry.path}/${next} (encountered while adding ${originalPath})`,
+        );
+      }
+      childNode.doAdd(pathParts.slice(1), entry, originalPath);
+    }
+  }
+
+  print(indentation = "") {
+    console.log(indentation + this.entry.path);
+    if (this.entry.type === FileType.Directory) {
+      this.entry.children.forEach((child) => child.print(indentation + " "));
+    }
+  }
 }
---
+++ src/browser/indexedDB.ts
@@ -0,0 +1,232 @@
+// From https://github.com/microsoft/vscode/blob/main/src/vs/base/browser/indexedDB.ts
+class MissingStoresError extends Error {
+  constructor(readonly db: IDBDatabase) {
+    super("Missing stores");
+  }
+}
+
+export class DBClosedError extends Error {
+  readonly code = "DBClosed";
+  constructor(dbName: string) {
+    super(`IndexedDB database '${dbName}' is closed.`);
+  }
+}
+
+export class IndexedDB {
+  static async create(
+    name: string,
+    version: number | undefined,
+    stores: string[],
+  ): Promise<IndexedDB> {
+    const database = await IndexedDB.openDatabase(name, version, stores);
+    return new IndexedDB(database, name);
+  }
+
+  private static async openDatabase(
+    name: string,
+    version: number | undefined,
+    stores: string[],
+  ): Promise<IDBDatabase> {
+    try {
+      return await IndexedDB.doOpenDatabase(name, version, stores);
+    } catch (err) {
+      if (err instanceof MissingStoresError) {
+        console.info(`Attempting to recreate the IndexedDB once.`, name);
+
+        try {
+          // Try to delete the db
+          await IndexedDB.deleteDatabase(err.db);
+        } catch (error) {
+          console.error(
+            `Error while deleting the IndexedDB`,
+            getErrorMessage(error),
+          );
+          throw error;
+        }
+
+        return await IndexedDB.doOpenDatabase(name, version, stores);
+      }
+
+      throw err;
+    }
+  }
+
+  private static doOpenDatabase(
+    name: string,
+    version: number | undefined,
+    stores: string[],
+  ): Promise<IDBDatabase> {
+    return new Promise((c, e) => {
+      const request = self.indexedDB.open(name, version);
+      request.onerror = (): void => e(request.error);
+      request.onsuccess = (): void => {
+        const db = request.result;
+        for (const store of stores) {
+          if (!db.objectStoreNames.contains(store)) {
+            console.error(
+              `Error while opening IndexedDB. Could not find '${store}'' object store`,
+            );
+            e(new MissingStoresError(db));
+            return;
+          }
+        }
+        c(db);
+      };
+      request.onupgradeneeded = (): void => {
+        const db = request.result;
+        for (const store of stores) {
+          if (!db.objectStoreNames.contains(store)) {
+            db.createObjectStore(store);
+          }
+        }
+      };
+    });
+  }
+
+  private static deleteDatabase(indexedDB: IDBDatabase): Promise<void> {
+    return new Promise((c, e) => {
+      // Close any opened connections
+      indexedDB.close();
+
+      // Delete the db
+      const deleteRequest = self.indexedDB.deleteDatabase(indexedDB.name);
+      deleteRequest.onerror = (): void => e(deleteRequest.error);
+      deleteRequest.onsuccess = (): void => c();
+    });
+  }
+
+  private database: IDBDatabase | null = null;
+  private readonly pendingTransactions: IDBTransaction[] = [];
+
+  constructor(database: IDBDatabase, private readonly name: string) {
+    this.database = database;
+  }
+
+  hasPendingTransactions(): boolean {
+    return this.pendingTransactions.length > 0;
+  }
+
+  close(): void {
+    if (this.pendingTransactions.length) {
+      this.pendingTransactions
+        .splice(0, this.pendingTransactions.length)
+        .forEach((transaction) => transaction.abort());
+    }
+    this.database?.close();
+    this.database = null;
+  }
+
+  runInTransaction<T>(
+    store: string,
+    transactionMode: IDBTransactionMode,
+    dbRequestFn: (store: IDBObjectStore) => IDBRequest<T>[],
+  ): Promise<T[]>;
+  runInTransaction<T>(
+    store: string,
+    transactionMode: IDBTransactionMode,
+    dbRequestFn: (store: IDBObjectStore) => IDBRequest<T>,
+  ): Promise<T>;
+  async runInTransaction<T>(
+    store: string,
+    transactionMode: IDBTransactionMode,
+    dbRequestFn: (store: IDBObjectStore) => IDBRequest<T> | IDBRequest<T>[],
+  ): Promise<T | T[]> {
+    if (!this.database) {
+      throw new DBClosedError(this.name);
+    }
+    const transaction = this.database.transaction(store, transactionMode);
+    this.pendingTransactions.push(transaction);
+    return new Promise<T | T[]>((c, e) => {
+      transaction.oncomplete = (): void => {
+        if (Array.isArray(request)) {
+          c(request.map((r) => r.result));
+        } else {
+          c(request.result);
+        }
+      };
+      transaction.onerror = (): void => e(transaction.error);
+      transaction.onabort = (): void => e(transaction.error);
+      const request = dbRequestFn(transaction.objectStore(store));
+    }).finally(() =>
+      this.pendingTransactions.splice(
+        this.pendingTransactions.indexOf(transaction),
+        1,
+      ),
+    );
+  }
+
+  async getKeyValues<V>(
+    store: string,
+    isValid: (value: unknown) => value is V,
+  ): Promise<Map<string, V>> {
+    if (!this.database) {
+      throw new DBClosedError(this.name);
+    }
+    const transaction = this.database.transaction(store, "readonly");
+    this.pendingTransactions.push(transaction);
+    return new Promise<Map<string, V>>((resolve) => {
+      const items = new Map<string, V>();
+
+      const objectStore = transaction.objectStore(store);
+
+      // Open a IndexedDB Cursor to iterate over key/values
+      const cursor = objectStore.openCursor();
+      if (!cursor) {
+        return resolve(items); // this means the `ItemTable` was empty
+      }
+
+      // Iterate over rows of `ItemTable` until the end
+      cursor.onsuccess = (): void => {
+        if (cursor.result) {
+          // Keep cursor key/value in our map
+          if (isValid(cursor.result.value)) {
+            items.set(cursor.result.key.toString(), cursor.result.value);
+          }
+
+          // Advance cursor to next row
+          cursor.result.continue();
+        } else {
+          resolve(items); // reached end of table
+        }
+      };
+
+      // Error handlers
+      const onError = (error: Error | null): void => {
+        console.error(`IndexedDB getKeyValues(): ${error?.message ?? ""}`);
+
+        resolve(items);
+      };
+      cursor.onerror = (): void => onError(cursor.error);
+      transaction.onerror = (): void => onError(transaction.error);
+    }).finally(() =>
+      this.pendingTransactions.splice(
+        this.pendingTransactions.indexOf(transaction),
+        1,
+      ),
+    );
+  }
+}
+
+export function getErrorMessage(err: unknown): string {
+  if (!err) {
+    return "Error";
+  }
+
+  if (
+    typeof err === "object" &&
+    "message" in err &&
+    typeof err.message === "string"
+  ) {
+    return err.message;
+  }
+
+  if (
+    typeof err === "object" &&
+    "stack" in err &&
+    typeof err.stack === "string"
+  ) {
+    return err.stack.split("\n")[0];
+  }
+
+  return String(err);
+}
